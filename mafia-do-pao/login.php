78680e06d83346ca7c27129a1484338b06675c12
Pedro Cortez <pedrohcortezlopes@gmail.com>
2024-06-25 11:37:43 -0300

    25/06
    Alterações feitas no dia 25/06 em sala de aula

--- mafia-do-pao/login.php
+++ mafia-do-pao/login.php
@@ -1,6 +1,35 @@
 <?php
 
+include("conectadb.php");
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $login = $_POST['txtlogin'];
+    $senha = $_POST['txtsenha'];
+
+    // COMEÇA VALIDAR BANCO DE DADOS
+    $sql = "SELECT COUNT(usu_id) FROM tb_usuarios
+    WHERE usu_login = '$login' AND usu_senha = '$senha' AND
+    usu_status = '1'";
+
+    // RETORNO DO BANCO
+    $retorno = mysqli_query($link, $sql);
+
+    while($tbl = mysqli_fetch_array($retorno))
+    {
+        $contagem = $tbl[0];
+    }
+
+    // VERIFICA SE NATAN EXISTE
+    if($contagem == 1)
+    {
+        echo"<script>window.location.href='home.php';</script>";
+    }
+    else
+    {
+        echo"<script>window.alert('USUARIO OU SENHA INCORRETOS');</script>";
+    }
+}
 
 ?>
 
@@ -17,6 +46,7 @@
 
     <div class="container-global">
         <form class="formulario" action="login.php" method="post">
+            <img src="img/logo.jfif" width=100px height=100px>
             <label>LOGIN</label>
             <input type="text" name="txtlogin" required>
             <br>
